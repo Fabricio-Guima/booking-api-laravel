6366a2ce6a15ffbcd50826d697603401deb47d7f
Fabricio_Guima <fsgkof@gmail.com>
2025-05-04 14:00:15 -0300

    add profile relationship

--- app/Models/User.php
+++ app/Models/User.php
@@ -52,4 +52,9 @@ class User extends Authenticatable
     {
         return $this->belongsTo(Role::class);
     }
+
+    public function profile()
+    {
+        return $this->hasOne(UserProfile::class);
+    }
 }
