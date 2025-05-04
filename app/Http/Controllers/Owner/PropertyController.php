dcdc0eec6f510a98acb19ab49f73458e5e264559
Fabricio_Guima <fsgkof@gmail.com>
2025-05-03 22:37:22 -0300

    PropertyController

---
+++ app/Http/Controllers/Owner/PropertyController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Owner;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+
+class PropertyController extends Controller
+{
+    use AuthorizesRequests;
+    public function index()
+    {
+        $this->authorize('properties-manage');
+
+        // Will implement property management later
+        return response()->json(['success' => true]);
+    }
+}
