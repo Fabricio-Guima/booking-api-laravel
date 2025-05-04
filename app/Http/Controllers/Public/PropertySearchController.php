3544d55da472c3df37f605e77b86cc5c80104d4b
Fabricio_Guima <fsgkof@gmail.com>
2025-05-04 19:19:01 -0300

    PropertySearchController

---
+++ app/Http/Controllers/Public/PropertySearchController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Public;
+
+use App\Http\Controllers\Controller;
+use App\Models\Property;
+use Illuminate\Http\Request;
+
+class PropertySearchController extends Controller
+{
+    public function __invoke(Request $request)
+    {
+        return Property::with('city')
+            ->when($request->city_id, function($query) use ($request) {
+                $query->where('city_id', $request->city_id);
+            })
+            ->when($request->country_id, function($query) use ($request) {
+                $query->whereHas('city', fn($q) => $q->where('country_id', $request->country_id));
+            })
+            ->get();
+    }
+}
