372d2ae920ed810f4e2a741533fee401e3ef592a
Fabricio_Guima <fsgkof@gmail.com>
2025-05-03 22:37:53 -0300

    BookingController

---
+++ app/Http/Controllers/User/BookingController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+
+class BookingController extends Controller
+{
+    use AuthorizesRequests;
+    public function index()
+    {
+        $this->authorize('bookings-manage');
+
+        // Will implement booking management later
+        return response()->json(['success' => true]);
+
+    }
+}
