7d03d557db86f6f6300584c0a9dc55bed367da3f
Fabricio_Guima <fsgkof@gmail.com>
2025-05-04 16:57:56 -0300

    create login and properties routes

--- routes/api.php
+++ routes/api.php
@@ -1,16 +1,20 @@
 <?php
 
+use App\Http\Controllers\Auth\{
+    LoginController, RegisterController
+};
 use App\Http\Controllers\Owner\PropertyController;
 use App\Http\Controllers\User\BookingController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::post('auth/register', App\Http\Controllers\Auth\RegisterController::class);
+Route::post('auth/login', LoginController::class);
+Route::post('auth/register', RegisterController::class);
 
 Route::middleware(['auth:sanctum', 'check-permission-of-user-middleware'])->group(function() {
 
-    Route::get('owner/properties',
-        [PropertyController::class, 'index']);
-    Route::get('user/bookings',
-        [BookingController::class, 'index']);
+    Route::get('owner/properties',[PropertyController::class, 'index']);
+    Route::post('owner/properties', [PropertyController::class, 'store']);
+
+    Route::get('user/bookings',[BookingController::class, 'index']);
 });
