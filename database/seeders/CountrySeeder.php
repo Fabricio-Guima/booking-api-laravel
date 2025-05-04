6bd1b785e4faf432d716fda5d0ff1970ee12f7ac
Fabricio_Guima <fsgkof@gmail.com>
2025-05-04 16:56:32 -0300

    CountrySeeder

--- database/seeders/CountrySeeder.php
+++ database/seeders/CountrySeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use App\Models\Country;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class CountrySeeder extends Seeder
@@ -13,15 +12,19 @@ class CountrySeeder extends Seeder
      */
     public function run(): void
     {
-        Country::create([
-            'name' => 'United States',
-            'lat' => 37.09024,
-            'long' => -95.712891
-        ]);
-        Country::create([
-            'name' => 'United Kingdom',
-            'lat' => 55.378051,
-            'long' => -3.435973
-        ]);
+        Country::updateOrCreate(
+            ['name' => 'United States'],
+            ['lat' => 37.09024, 'long' => -95.712891]
+        );
+
+        Country::updateOrCreate(
+            ['name' => 'United Kingdom'],
+            ['lat' => 55.378051, 'long' => -3.435973]
+        );
+
+        Country::updateOrCreate(
+            ['name' => 'Brazil'],
+            ['lat' => -14.235004, 'long' => -51.92528]
+        );
     }
 }
