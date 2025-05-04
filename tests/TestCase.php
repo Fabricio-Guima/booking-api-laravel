6515e3cb006213ecdcf9731816356e861f3d6491
Fabricio_Guima <fsgkof@gmail.com>
2025-05-03 22:39:54 -0300

    Loading all seeders to work the tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,18 @@
 
 namespace Tests;
 
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use \Tests\CreatesApplication;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    use RefreshDatabase;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->seed();
+    }
 }
