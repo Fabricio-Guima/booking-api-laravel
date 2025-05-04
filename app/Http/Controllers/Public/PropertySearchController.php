0b5c86de5d7e08ed5cb50361fa81b2597474ebc7
Fabricio_Guima <fsgkof@gmail.com>
2025-05-04 19:54:04 -0300

    Add coordinate search to query

--- app/Http/Controllers/Public/PropertySearchController.php
+++ app/Http/Controllers/Public/PropertySearchController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Public;
 
 use App\Http\Controllers\Controller;
+use App\Models\Geoobject;
 use App\Models\Property;
 use Illuminate\Http\Request;
 
@@ -11,11 +12,25 @@ class PropertySearchController extends Controller
     public function __invoke(Request $request)
     {
         return Property::with('city')
-            ->when($request->city_id, function($query) use ($request) {
-                $query->where('city_id', $request->city_id);
+            ->when($request->city, function($query) use ($request) {
+                $query->where('city_id', $request->city);
             })
             ->when($request->country_id, function($query) use ($request) {
                 $query->whereHas('city', fn($q) => $q->where('country_id', $request->country_id));
+            })
+            ->when($request->geoobject, function($query) use ($request) {
+                $geoobject = Geoobject::find($request->geoobject);
+                if ($geoobject) {
+                    $condition = "(
+                        6371 * acos(
+                            cos(radians(" . $geoobject->lat . "))
+                            * cos(radians(`lat`))
+                            * cos(radians(`long`) - radians(" . $geoobject->long . "))
+                            + sin(radians(" . $geoobject->lat . ")) * sin(radians(`lat`))
+                        ) < 10
+                    )";
+                    $query->whereRaw($condition);
+                }
             })
             ->get();
     }
