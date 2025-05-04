c8cebdf984c754c7ec0b63f19ae92430f92e77f5
Fabricio_Guima <fsgkof@gmail.com>
2025-05-03 22:39:33 -0300

    create protected routes using middlewares

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,16 @@
 <?php
 
+use App\Http\Controllers\Owner\PropertyController;
+use App\Http\Controllers\User\BookingController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('auth/register', App\Http\Controllers\Auth\RegisterController::class);
+
+Route::middleware(['auth:sanctum', 'check-permission-of-user-middleware'])->group(function() {
+
+    Route::get('owner/properties',
+        [PropertyController::class, 'index']);
+    Route::get('user/bookings',
+        [BookingController::class, 'index']);
+});
