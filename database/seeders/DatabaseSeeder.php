a65b95a6865dc5e7c46796b9f7f04868869913f9
Fabricio_Guima <fsgkof@gmail.com>
2025-05-05 14:58:26 -0300

    add PropertySeeder and ApartmentSeeder to run

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Apartment;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -19,6 +20,9 @@ class DatabaseSeeder extends Seeder
 
         $this->call(CountrySeeder::class);
         $this->call(CitySeeder::class);
+        $this->call(PropertySeeder::class);
         $this->call(GeoobjectSeeder::class);
+        $this->call(ApartmentSeeder::class);
+        // apartment_type is created in ApartmentType migration
     }
 }
