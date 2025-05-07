0e136c2f3cb484bb902e2bbc2c145b1f24512e8a
Fabricio_Guima <fsgkof@gmail.com>
2025-05-07 01:01:34 -0300

    add rooms relationship

--- app/Models/RoomType.php
+++ app/Models/RoomType.php
@@ -10,4 +10,9 @@ class RoomType extends Model
     use HasFactory;
 
     protected $fillable = ['name'];
+
+    public function rooms()
+    {
+        return $this->hasMany(Room::class);
+    }
 }
