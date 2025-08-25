decd3ac536e3c4ed8be40e498d6af5eec846180c
Fabricio_Guima <fsgkof@gmail.com>
2025-08-24 21:08:02 -0300

    PropertySeeder

--- database/seeders/PropertySeeder.php
+++ database/seeders/PropertySeeder.php
@@ -12,11 +12,21 @@ class PropertySeeder extends Seeder
      */
     public function run(): void
     {
-        Property::create([
+        Property::firstOrCreate([
             'name' => 'Minha Propriedade',
             'city_id' => 3,
             'owner_id' => 1,
             'address_street' => 'avenida silveira martins, casa 3, lote 7',
+            'address_postcode' => '25585540',
+            'lat' => -23.5505,
+            'long' => -46.6333,
+        ]);
+
+        Property::firstOrCreate([
+            'name' => 'Propriedade 2',
+            'city_id' => 3,
+            'owner_id' => 1,
+            'address_street' => 'Rua ali perto',
             'address_postcode' => '25585540',
             'lat' => -23.5505,
             'long' => -46.6333,
